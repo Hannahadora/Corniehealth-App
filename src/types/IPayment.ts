5af66ae34ad15c147ab76adacab4b47771433dfb
dino16m <anselem16m@gmail.com>
2021-07-09 10:10:27 +0100

    Change indent

--- src/types/IPayment.ts
+++ src/types/IPayment.ts
@@ -1,7 +1,7 @@
-export default interface IPayment{
-    accountName: string;
-    accountNumber: string;
-    bank: string;
-    paymentCategories: string;
-    location: string;
-  }
+export default interface IPayment {
+  accountName: string;
+  accountNumber: string;
+  bank: string;
+  paymentCategories: string;
+  location: string;
+}
