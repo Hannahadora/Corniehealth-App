2f0d3e8619014dfaffb966693a9003844915d171
Dino16m <37278830+Dino16m@users.noreply.github.com>
2021-07-15 09:54:55 +0100

    Fix corniehealth url on http.ts file

--- src/plugins/http.ts
+++ src/plugins/http.ts
@@ -14,7 +14,7 @@ export const quantumClient = () =>
     "http://18.132.188.41:7000"
   );
 
-const cornieUrl = "http://localhost:7000";
+const cornieUrl = "https://corniehealth.herokuapp.com";
 export const cornieClient = () =>
   new JSONClient(
     {
