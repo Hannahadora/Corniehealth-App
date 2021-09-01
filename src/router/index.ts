a006edc29adc96d5dbe8c348ebe2fd62a80cb340
Ogunrinde Eunice Modupe <31671705+PenielShell@users.noreply.github.com>
2021-09-01 14:28:08 +0100

    Updates on landing page

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,7 @@ const routes: Array<RouteRecordRaw> = [
     path: "/",
     name: "Home",
     component: Home,
-    redirect: "/dashboard",
+    // redirect: "/dashboard",
   },
   {
     path: "/signup",
