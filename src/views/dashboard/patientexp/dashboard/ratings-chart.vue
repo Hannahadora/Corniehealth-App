6edaa79f59ec06026a1be8d996d3f796a88fa579
dino16m <anselem16m@gmail.com>
2021-09-05 18:30:37 +0100

    Implement rating chart

--- src/views/dashboard/patientexp/dashboard/ratings-chart.vue
+++ src/views/dashboard/patientexp/dashboard/ratings-chart.vue
@@ -1,9 +1,61 @@
 <template>
   <chart-card height="439px" title="Rating">
-    <div class="w-full grid grid-cols-3">
-      <span class="block p-7">
+    <div class="w-full grid rating-grid">
+      <span class="block p-3">
         <canvas id="rating-chart"></canvas>
       </span>
+      <div class="w-full mt-5 flex flex-col">
+        <rating-bar :percent="12" color="warning" title="Neutral Responses" />
+        <rating-bar
+          :percent="30"
+          class="mt-3"
+          color="danger"
+          subtitle="(+3 IN THE LAST 30 DAYS)"
+          title="Negative Responses"
+        />
+        <rating-bar
+          :percent="58"
+          class="mt-3"
+          color="success"
+          subtitle="(14 RATINGS)"
+          title="Positive Responses"
+        />
+        <span class="mt-16 block">
+          <hr class="w-full border" />
+          <span class="w-full flex justify-between">
+            <div class="mt-3 flex items-center border-r-2 pr-3">
+              <span class="font-bold text-success">5</span>
+              <span class="ml-1 font-semibold text-sm text-black">
+                Very Satisfied
+              </span>
+            </div>
+            <div class="mt-3 flex items-center border-r-2 pr-3">
+              <span class="font-bold text-success">2</span>
+              <span class="ml-1 font-semibold text-sm text-black">
+                Satisfied
+              </span>
+            </div>
+            <div class="mt-3 flex items-center border-r-2 pr-3">
+              <span class="font-bold text-success">4</span>
+              <span class="ml-1 font-semibold text-sm text-black">
+                Average
+              </span>
+            </div>
+            <div class="mt-3 flex items-center border-r-2 pr-3">
+              <span class="font-bold text-success">3</span>
+              <span class="ml-1 font-semibold text-sm text-black">
+                Satisfied
+              </span>
+            </div>
+            <div class="mt-3 flex items-center pr-3">
+              <span class="font-bold text-success">0</span>
+              <span class="ml-1 font-semibold text-sm text-black">
+                Very Dissatisfied
+              </span>
+            </div>
+          </span>
+        </span>
+      </div>
     </div>
   </chart-card>
 </template>
@@ -12,6 +64,8 @@ import { Options, Vue } from "vue-class-component";
 import ChartCard from "./chart-card.vue";
 import { Chart } from "chart.js";
 import { CustomDoughnutController } from "@/plugins/chart";
+import ProgressBar from "@/components/progress-bar.vue";
+import RatingBar from "./rating-bar.vue";
 
 Chart.register(CustomDoughnutController);
 
@@ -19,6 +73,8 @@ Chart.register(CustomDoughnutController);
   name: "RatingChart",
   components: {
     ChartCard,
+    ProgressBar,
+    RatingBar,
   },
 })
 export default class RatingChart extends Vue {
@@ -38,13 +94,13 @@ export default class RatingChart extends Vue {
       ],
     };
     const ctx: any = document.getElementById("rating-chart");
-    ctx.height = 150;
     this.chart?.destroy();
     this.chart = new Chart(ctx, {
       type: "derivedDoughnut",
       data,
       options: {
         cutout: "70%",
+        maintainAspectRatio: false,
         elements: {
           center: {
             text: "4.1/5 Average",
@@ -66,6 +122,10 @@ export default class RatingChart extends Vue {
         },
       },
     });
+    if (this.chart && this.chart.canvas.parentNode) {
+      const parent: any = this.chart.canvas.parentNode;
+      parent.style.height = "350px";
+    }
   }
 }
 </script>
