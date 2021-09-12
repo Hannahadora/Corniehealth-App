ea698e26aa99eab07ff69f5093c1d1c8d0a5a62f
dino16m <anselem16m@gmail.com>
2021-09-12 19:28:42 +0100

    Add chart filter

--- src/views/dashboard/patientexp/patients/chart-filter.ts
+++ src/views/dashboard/patientexp/patients/chart-filter.ts
@@ -14,14 +14,33 @@ const Months = [
   "Nov",
   "Dec",
 ];
-const Weekdays: [string, number][] = [
-  ["Mon", 1],
-  ["Tue", 2],
-  ["Wed", 3],
-  ["Thu", 4],
-  ["Fri", 5],
-  ["Sat", 6],
-  ["Sun", 7],
+const Weekdays: string[] = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+const HoursOfDay: string[] = [
+  "00:00",
+  "01:00",
+  "02:00",
+  "03:00",
+  "04:00",
+  "05:00",
+  "06:00",
+  "07:00",
+  "08:00",
+  "09:00",
+  "10:00",
+  "11:00",
+  "12:00",
+  "13:00",
+  "14:00",
+  "15:00",
+  "16:00",
+  "17:00",
+  "18:00",
+  "19:00",
+  "20:00",
+  "21:00",
+  "22:00",
+  "23:00",
 ];
 
 type Order = "Today" | "WTD" | "MTD" | "YTD";
@@ -30,23 +49,45 @@ interface DateStat {
   count: number;
   date: Date;
 }
-function sortDaily(data: DateStat[]) {
-  const dailyMap = new Map<number, number>();
-  data.forEach((d) => {
-    const day = d.date.getDay();
-    const dayCount = Number(d.count);
-    const sum = (dailyMap.get(day) || 0) + dayCount;
-    dailyMap.set(day, sum);
-  });
 
-  const dataSet: number[] = [];
-  const labels: string[] = [];
-  Weekdays.forEach(([day, id]) => {
-    const sum = dailyMap.get(id) || 0;
-    dataSet.push(sum);
-    labels.push(day);
+function getDaysInMonth() {
+  const date = new Date();
+  date.setDate(1);
+  const days = [];
+  const thisMonth = date.getMonth();
+  while (date.getMonth() === thisMonth) {
+    const day = `${new Date(date).getDate()}`;
+    days.push(day);
+    date.setDate(date.getDate() + 1);
+  }
+  return days;
+}
+
+function getUnitData(data: DateStat[], getUnit: (date: Date) => number) {
+  const map = new Map<number, number>();
+  data.forEach((d) => {
+    const day = getUnit(d.date);
+    const dayCount = Number(d.count);
+    const sum = (map.get(day) || 0) + dayCount;
+    map.set(day, sum);
   });
-  const total = dataSet.reduce((prev, curr) => prev + curr, 0);
+  return map;
+}
+
+function getChartData(
+  data: DateStat[],
+  labels: string[],
+  getUnit: (date: Date) => number
+) {
+  const unitData = getUnitData(data, getUnit);
+  const dataSet: number[] = [];
+  let total = 0;
+  labels.forEach((_, index) => {
+    const unit = index + 1;
+    const sum = unitData.get(unit) || 0;
+    dataSet.push(sum);
+    total += sum;
+  });
   return {
     dataSet,
     labels,
@@ -54,7 +95,42 @@ function sortDaily(data: DateStat[]) {
   };
 }
 
+function weekToDay(data: DateStat[]) {
+  return getChartData(data, Weekdays, (d) => d.getDay());
+}
+
+function yearToDay(data: DateStat[]) {
+  return getChartData(data, Months, (d) => d.getMonth());
+}
+
+function monthToDay(data: DateStat[]) {
+  return getChartData(data, getDaysInMonth(), (d) => d.getDate());
+}
+
+function today(data: DateStat[]) {
+  const today = new Date();
+  const todayData = data.filter(
+    (d) =>
+      d.date.getFullYear() == today.getFullYear() &&
+      d.date.getMonth() == today.getMonth() &&
+      d.date.getDate() == today.getDate()
+  );
+  return getChartData(todayData, HoursOfDay, (d) => d.getHours());
+}
+
 export function groupData(data: IStat[], order: Order) {
   const datedData = data.map((d) => ({ ...d, date: new Date(d.date) }));
-  return sortDaily(datedData);
+  switch (order) {
+    case "WTD":
+      return weekToDay(datedData);
+    case "YTD":
+      return yearToDay(datedData);
+    case "MTD":
+      return monthToDay(datedData);
+    case "Today":
+      return today(datedData);
+    default:
+      break;
+  }
+  return weekToDay(datedData);
 }
