60821d833ab925582a135f4f87467a292cb0fae6
fmshot2 <femishotolaa@gmail.com>
2021-10-19 20:17:24 +0100

    progress notes ts file

---
+++ src/store/progressnote/index.ts
@@ -0,0 +1,56 @@
+import ObjectSet from "@/lib/objectset";
+import IAllergy from "@/types/IAllergy";
+import { StoreOptions } from "vuex";
+import { deleteAllergy, fetchAllergys, getPractitioners } from "./helper";
+
+interface AllergyState {
+  allergys: IAllergy[];
+  practitioners: any[],
+}
+
+export default {
+  namespaced: true,
+  state: {
+    allergys: [],
+    practitioners: [],
+  },
+  mutations: {
+    setAllergys(state, allergys: IAllergy[]) {
+      state.allergys = [...allergys];
+    },
+    setPractitioners(state, pts) {
+      if (pts && pts.length > 0) state.practitioners = [ ...pts ];
+    },
+    updateAllergys(state, allergys: IAllergy[]) {
+      const allergySet = new ObjectSet([...state.allergys, ...allergys], "id");
+      state.allergys = [...allergySet];
+    },
+    deleteAllergy(state, id: string) {
+      const index = state.allergys.findIndex((allergy) => allergy.id == id);
+      if (index < 0) return;
+      const allergys = [...state.allergys];
+      allergys.splice(index, 1);
+      state.allergys = [...allergys];
+    },
+  },
+  actions: {
+    async fetchAllergys(ctx,patientId:string) {
+      const allergys = await fetchAllergys(patientId);
+      ctx.commit("setAllergys", allergys);
+    },
+    async getPractitioners(ctx) {
+      const pts = await getPractitioners();      
+      ctx.commit("setPractitioners", pts);
+    },
+    async getAllergyById(ctx, id: string) {
+      if (ctx.state.allergys.length < 1) await ctx.dispatch("fetchAllergys");
+      return ctx.state.allergys.find((allergy) => allergy.id == id);
+    },
+    async deleteAllergy(ctx, id: string) {
+      const deleted = await deleteAllergy(id);
+      if (!deleted) return false;
+      ctx.commit("deleteAllergy", id);
+      return true;
+    },
+  },
+} as StoreOptions<AllergyState>;
--- src/views/dashboard/ehr/progressnotes/add-condition.vue
+++ src/views/dashboard/ehr/progressnotes/add-condition.vue
@@ -27,7 +27,17 @@
 
         <div class="grid grid-cols-2 text-center gap-3 mt-5">
           <cornie-input v-model="stageChiefComplaint" label="Chief Complaint" />
+          <div class="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+    <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20"><path d="M14.243 5.757a6 6 0 10-.986 9.284 1 1 0 111.087 1.678A8 8 0 1118 10a3 3 0 01-4.8 2.401A4 4 0 1114 10a1 1 0 102 0c0-1.537-.586-3.07-1.757-4.243zM12 10a2 2 0 10-4 0 2 2 0 004 0z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
+  </div>
         </div>
+
+<div class="relative text-gray-700">
+  <input class="w-full h-10 pl-3 pr-8 text-base focus:outline-none  placeholder-gray-600 border rounded-lg focus:shadow-outline" type="text" placeholder="Regular input"/>
+  <div class="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+    <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20"><path d="M14.243 5.757a6 6 0 10-.986 9.284 1 1 0 111.087 1.678A8 8 0 1118 10a3 3 0 01-4.8 2.401A4 4 0 1114 10a1 1 0 102 0c0-1.537-.586-3.07-1.757-4.243zM12 10a2 2 0 10-4 0 2 2 0 004 0z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
+  </div>
+</div>
 
         <div class="flex items-center justify-between mt-3 mb-2">
           <h3 class="text-sm">History of Present Sickness (HPI)</h3>
