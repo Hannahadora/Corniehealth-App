dba380dbfa47fefc54759c57bf5abf78237263d0
dino16m <anselem16m@gmail.com>
2021-11-24 10:07:09 +0100

    Fix account activation race condition

--- src/views/auth/signup/activateaccount.vue
+++ src/views/auth/signup/activateaccount.vue
@@ -59,22 +59,30 @@
         </ul>
       </div>
       <div class="w-full">
-       <cornie-btn
-            class="font-semibold rounded-full bg-danger mt-3 w-full text-white p-2 "
-             :disabled="!emailVerified"
+        <cornie-btn
+          class="
+            font-semibold
+            rounded-full
+            bg-danger
+            mt-3
+            w-full
+            text-white
+            p-2
+          "
+          :disabled="!emailVerified"
           :loading="loading"
           type="submit"
           :class="{ 'bg-gray-600': !emailVerified || !valid }"
-            >
-            Create Account
-          </cornie-btn>
+        >
+          Create Account
+        </cornie-btn>
       </div>
     </form>
   </div>
 </template>
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import { Prop,PropSync } from "vue-property-decorator";
+import { Prop, PropSync } from "vue-property-decorator";
 import store from "@/store/";
 import { cornieClient, quantumClient } from "@/plugins/http";
 import PasswordInput from "@/components/PasswordInput.vue";
@@ -101,12 +109,9 @@ export default class ActivateAccount extends Vue {
   @Prop({ required: false, default: false })
   emailVerified!: boolean;
 
-
   @PropSync("user", { required: false })
   userSync!: CreatedUser;
   user = {} as CreatedUser;
-
-
 
   password = "";
   confirmation = "";
@@ -118,7 +123,7 @@ export default class ActivateAccount extends Vue {
   @user.State
   cornieData!: any;
 
- setUser(payload: any) {
+  setUser(payload: any) {
     this.userSync = {
       id: payload.userId,
       email: payload.email,
@@ -205,17 +210,16 @@ export default class ActivateAccount extends Vue {
         "/auth/account/activate/",
         this.payload
       );
-      
-      if (!data.success){
-         this.showText = true; 
-         return window.notify({ msg: errMsg });
+
+      if (!data.success) {
+        this.showText = true;
+        return window.notify({ msg: errMsg });
       }
       store.commit("user/setLoginInfo", data);
+      await this.saveCornieData();
       this.$router.replace("/dashboard");
-      this.saveCornieData();
     } catch (error) {
-
-       window.notify({ msg: "Account not activated" });
+      window.notify({ msg: "Account not activated" });
     }
   }
 }
