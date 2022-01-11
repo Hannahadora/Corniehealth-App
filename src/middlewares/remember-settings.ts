70c9e41f345bf94e8ce1c8f22cd7787a235d3475
dino16m <anselem16m@gmail.com>
2022-01-11 08:49:12 +0100

    add route suggestion in recents

---
+++ src/middlewares/remember-settings.ts
@@ -0,0 +1,13 @@
+import { suggester } from "@/plugins/route-suggester";
+import { RouteLocationNormalized } from "vue-router";
+
+export async function remeberSettings(
+  to: RouteLocationNormalized,
+  _: RouteLocationNormalized,
+  __: any
+) {
+  if (!to.matched.some((record) => record.meta.settings)) return;
+  if (!to.name) return;
+  suggester.rememberRoute(to.fullPath, to.name as string);
+  console.log("Remembering this ", to.fullPath, to.name);
+}
