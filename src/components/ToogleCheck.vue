d8f9c15ce2fd079f3135a6023a3c4ac501b0c6cf
dino16m <anselem16m@gmail.com>
2022-02-16 08:02:02 +0100

    Update account security

--- src/components/ToogleCheck.vue
+++ src/components/ToogleCheck.vue
@@ -1,81 +1,19 @@
 <template>
   <div>
-    <div class="toogle-check -ml-4 flex justify-center items-center">
-      <span
-        class="text-gray font-semibold mr-0 pb-1.5"
-        :class="{ 'text-gray-200': modelSync }"
-      >
-        {{ uncheckedText }}</span
-      >
-      <label class="switch">
+    <div class="toogle-check flex items-center">
+      <label class="text-xs" :class="{ 'text-gray-400': modelSync }">
+        {{ uncheckedText }}
+      </label>
+      <label class="switch mx-0">
         <input type="checkbox" v-model="modelSync" />
         <span class="slider round"></span>
       </label>
-      <span
-        class="text-gray font-semibold ml-0 pb-1.5"
-        :class="{ 'text-gray-200': !modelSync }"
-      >
-        {{ checkedText }}</span
-      >
+      <label class="text-xs" :class="{ 'text-gray-400': !modelSync }">
+        {{ checkedText }}
+      </label>
     </div>
   </div>
 </template>
-
-<style scoped>
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 60px;
-  height: 34px;
-}
-.switch input {
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #35ba83;
-  -webkit-transition: 0.4s;
-  transition: 0.4s;
-}
-.slider:before {
-  position: absolute;
-  content: "";
-  height: 28px;
-  width: 28px;
-  left: 2px;
-  bottom: 4px;
-  background-color: white;
-  transition: 0.4s;
-}
-input:checked + .slider {
-  background: #fe4d3c;
-}
-
-.checked + * + input:checked {
-  color: #fe4d3c !important;
-}
-span.unchecked input:checked + .slider {
-  color: #fe4d3c;
-}
-input:checked + .slider:before {
-  -webkit-transform: translateX(26px);
-  -ms-transform: translateX(26px);
-  transform: translateX(26px);
-}
-.slider.round {
-  border-radius: 34px;
-}
-.slider.round:before {
-  border-radius: 50%;
-}
-</style>
 
 <script>
 export default {
