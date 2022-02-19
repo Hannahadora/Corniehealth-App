8211a7269a41d3fda527f5af91c2fabc1515b175
dino16m <anselem16m@gmail.com>
2022-02-19 11:27:06 +0100

    update dependencies

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import "./index.css";
 import { registerGlobalComponents } from "./components/globals";
 import "./middlewares";
 
+import "primevue/resources/themes/saga-blue/theme.css"; //theme
+import "primevue/resources/primevue.min.css"; //core css
+import "primeicons/primeicons.css";
 import VCalendar from "v-calendar";
 
 const app = createApp(App).use(router).use(store).use(VCalendar, {});
