63bd470335f09032db473397b139f311b1f5a286
Hannah Chiamaka <hannahadora97@gmail.com>
2022-04-21 19:13:29 +0100

    update on lab and pharmacy order

--- src/plugins/http.ts
+++ src/plugins/http.ts
@@ -23,6 +23,6 @@ export const cornieClient = () =>
       "Content-Type": "application/json",
       Authorization: authToken(),
     },
-    "https://dev.corniehealth.com"
+    "https://dev.corniehealth.com" || cornieUrl
   );
 
