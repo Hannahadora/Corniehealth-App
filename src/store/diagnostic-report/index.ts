b6849eeb43c18a75c97a09a27540306a9e01482d
Ogunrinde Eunice Modupe <31671705+PenielShell@users.noreply.github.com>
2022-05-24 14:28:46 +0100

    Updates on conditions

--- src/store/diagnostic-report/index.ts
+++ src/store/diagnostic-report/index.ts
@@ -23,6 +23,7 @@ export default {
           ctx.commit("setReport", r);
         })
         .catch(r => {
+          // eslint-disable-next-line no-console
           console.log("getRe", r);
         });
     },
@@ -32,6 +33,7 @@ export default {
           ctx.dispatch("getAllReports");
         })
         .catch(e => {
+          // eslint-disable-next-line no-console
           console.log("createR", e);
         });
     },
@@ -41,15 +43,18 @@ export default {
           ctx.dispatch("getAllReports");
         })
         .catch(d => {
+          // eslint-disable-next-line no-console
           console.log("updateR", d);
         });
     },
     async updateRStatus(ctx: any, payload: any) {
       await updateReportStatus(payload)
         .then(e => {
+          // eslint-disable-next-line no-console
           console.log("updateSuccess", e);
         })
         .catch(f => {
+          // eslint-disable-next-line no-console
           console.log("ds", f);
         });
     },
@@ -59,6 +64,7 @@ export default {
           ctx.dispatch("getAllReports");
         })
         .catch(e => {
+          // eslint-disable-next-line no-console
           console.log("deleteReport", e);
         });
     },
