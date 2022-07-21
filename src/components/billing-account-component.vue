71feb000db0a7d80023797a98c2af756fa2088fe
sukiejosh <oladokunjoshua2016@gmail.com>
2022-07-21 01:25:28 +0100

    fix billing account component

--- src/components/billing-account-component.vue
+++ src/components/billing-account-component.vue
@@ -1,52 +1,67 @@
 <template>
   <div class="flex flex-col">
-    <div class="flex items-center">
-      <div class="font-bold">{{ selectedAccount }}</div>
-      <div class="text-red-500">Change Billing Account</div>
+    <div class="flex items-center mb-3">
+      <div class="font-bold flex-1 capitalize">
+        {{ accountNumber }} - {{ selectedAccount }}
+      </div>
+      <div
+        @click="changeAccount = true"
+        class="text-red-500 cursor-pointer flex-none font-bold"
+      >
+        Change Billing Account
+      </div>
     </div>
     <cornie-select
       v-if="changeAccount"
       v-bind="$attrs"
-      :rules="required"
       :items="AllAccount"
-      v-model="selectedAccount"
-      label="Change Billing Account"
       placeholder="--Select--"
       class="w-full capitalize"
+      label="Change Billing Account"
+      @selected="update"
     />
   </div>
 </template>
 <script lang="ts">
+  import CornieSelect from "@/components/autocomplete.vue";
   import { cornieClient } from "@/plugins/http";
+  import IPaymentAccount from "@/types/IPaymentAccount";
+  import { nextTick } from "vue";
   import { Options, Vue } from "vue-class-component";
   import { Prop, PropSync } from "vue-property-decorator";
 
   @Options({
     name: "Billing account component",
+    components: {
+      CornieSelect,
+    },
   })
   export default class BillingAccountComponent extends Vue {
-    @PropSync("modelValue", { default: "insurance" })
+    @PropSync("modelValue")
     account!: string;
 
-    @Prop({ type: String, required: true })
-    patientId!: string;
+    @Prop({ required: true })
+    patientId!: any[];
 
-    allAccounts = [];
+    allAccounts: IPaymentAccount[] = [];
     changeAccount = false;
-    selectedAccount = "insurance";
+    selectedAccount: any = "insurance";
+    accountNumber: any = "";
 
     get AllAccount() {
       return this.allAccounts.length > 0
-        ? this.allAccounts.map((x: any) => {
+        ? this.allAccounts.map((x) => {
             return {
-              card: x.card,
+              code: x.id,
+              display: x.type,
             };
           })
         : [];
     }
 
     async getAccounts() {
-      const url = `/api/v1/patient/payment-account/patient/${this.patientId}`;
+      // if(!this.patientId)
+      const url = `/api/v1/patient/payment-account/patient/${this.patientId[0]?.patientId}`;
       try {
         const response = await cornieClient().get(url);
         if (response.success) {
@@ -60,7 +75,21 @@
       }
     }
 
+    update(paymentId: any) {
+      nextTick(() => {
+        console.log("payment id", paymentId);
+        this.accountNumber = this.allAccounts.find(
+          (x) => x.id == paymentId
+        )?.accountNo;
+        this.selectedAccount = this.allAccounts.find(
+          (x) => x.id == paymentId
+        )?.type;
+        this.$emit("update:modelValue", paymentId);
+      });
+    }
+
     async mounted() {
+      console.log("patient", this.patientId);
       await this.getAccounts();
     }
   }
--- src/views/dashboard/patientexp/calendar/appointments/index.vue
+++ src/views/dashboard/patientexp/calendar/appointments/index.vue
@@ -2,6 +2,8 @@
   <div class="h-full flex justify-center">
     <div class="w-full mx-5">
       <span class="w-full">
+        {{ account }}
+        <BillingAccountComponent v-model="account" />
         <appointment-empty-state v-if="empty" />
         <appointment-existing-state v-else />
       </span>
@@ -10,35 +12,35 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import IAppointment from "@/types/IAppointment";
-import { namespace } from "vuex-class";
-import AppointmentEmptyState from "./emptyState.vue";
-import AppointmentExistingState from "./existingState.vue";
+  import IAppointment from "@/types/IAppointment";
+  import { Options, Vue } from "vue-class-component";
+  import { namespace } from "vuex-class";
+  import AppointmentEmptyState from "./emptyState.vue";
+  import AppointmentExistingState from "./existingState.vue";
 
-const appointment = namespace("appointment");
+  const appointment = namespace("appointment");
 
-@Options({
-  name: "AppointmentIndex",
-  components: {
-    AppointmentEmptyState,
-    AppointmentExistingState,
-  },
-})
-export default class AppointmentIndex extends Vue {
+  @Options({
+    name: "AppointmentIndex",
+    components: {
+      AppointmentEmptyState,
+      AppointmentExistingState,
+    },
+  })
+  export default class AppointmentIndex extends Vue {
+    account = "";
+    get empty() {
+      return this.appointments.length < 1;
+    }
 
-  get empty() {
-    return this.appointments.length < 1;
+    @appointment.State
+    appointments!: IAppointment[];
+
+    @appointment.Action
+    fetchAppointments!: () => Promise<void>;
+
+    async created() {
+      await this.fetchAppointments();
+    }
   }
-
-  @appointment.State
-  appointments!: IAppointment[];
-
-  @appointment.Action
-  fetchAppointments!: () => Promise<void>;
-
-  async created() {
-    await this.fetchAppointments();
-  }
-}
 </script>
