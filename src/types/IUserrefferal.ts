98058e4b17b8246fccf31542665cb27db2796916
Ogunrinde Eunice Modupe <31671705+PenielShell@users.noreply.github.com>
2022-09-28 13:09:38 +0100

    Updates on issues log

--- src/types/IUserrefferal.ts
+++ src/types/IUserrefferal.ts
@@ -1,7 +1,13 @@
 export default interface IUserrefferal {
+    id?: string; 
     availableCredits: number;
     successfulReferrals: number;
     referralToken: string;
     pendingCredits: number;
+    transactionMade: boolean;
+    name: string;
+    email: string;
+    onboardDate: string;
+    status: string;
   }
   
